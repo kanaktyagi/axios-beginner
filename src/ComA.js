49b2d673b88f0a902b0f7382d3699eea22aeea92
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-29 16:14:28 +0530

    Displayed the final output

--- src/ComA.js
+++ src/ComA.js
@@ -3,16 +3,22 @@ import axios from 'axios'
 
 function ComA() {
      const [num, setNum] = useState();
+     const [name, setName] = useState();
+     const [ move, setMove] = useState();
      useEffect(() => {
         async function getData() {
             const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
-            console.log("res", res)
+            console.log("res", res.data.name)
+        setMove(res.data.moves.length)
+            setName(res.data.name)
         }
         getData() 
      })
     return (
         <div>
-            <h1> you choose {num} value</h1>
+            <h1> you choose <span style={{color: 'red'}}> {num} </span> value</h1>
+            <h1> my name <span style={{color: 'red'}}>{name} </span> value</h1>
+            <h1> I have <span style={{color: 'red'}}> {move} </span> value</h1>
             <select value={num} onChange={(event)=> {
                 setNum(event.target.value)
             }}>
@@ -20,7 +26,7 @@ function ComA() {
                 <option value='2'>2</option>
                 <option value='3'>3</option>
                 <option value='4'>4</option>
-                <option value='5'>6</option>
+                <option value='5'>5</option>
             </select>
         </div>
     )

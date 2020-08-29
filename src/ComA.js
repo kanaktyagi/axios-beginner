2eed7c96d452bd415e848040fd2441721495f25f
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-29 15:06:02 +0530

    Added a new component ComA

---
+++ src/ComA.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ComA() {
+    return (
+        <div>
+            <h1> Pokemon</h1>
+            <select>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>6</option>
+            </select>
+        </div>
+    )
+}
+
+export default ComA
